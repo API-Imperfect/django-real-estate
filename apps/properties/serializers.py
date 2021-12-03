005f3fdf77f31a20c338300e2db0cfa944949332
API Imperfect <alphaogilo@gmail.com>
2021-12-03 09:59:10 +0200

    test: all routes work in containers

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -8,6 +8,11 @@ from .models import Property, PropertyViews
 class PropertySerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     country = CountryField(name_only=True)
+    cover_photo = serializers.SerializerMethodField()
+    photo1 = serializers.SerializerMethodField()
+    photo2 = serializers.SerializerMethodField()
+    photo3 = serializers.SerializerMethodField()
+    photo4 = serializers.SerializerMethodField()
 
     class Meta:
         model = Property
@@ -43,6 +48,21 @@ class PropertySerializer(serializers.ModelSerializer):
 
     def get_user(self, obj):
         return obj.user.username
+
+    def get_cover_photo(self, obj):
+        return obj.cover_photo.url
+
+    def get_photo1(self, obj):
+        return obj.photo1.url
+
+    def get_photo2(self, obj):
+        return obj.photo2.url
+
+    def get_photo3(self, obj):
+        return obj.photo3.url
+
+    def get_photo4(self, obj):
+        return obj.photo4.url
 
 
 class PropertyCreateSerializer(serializers.ModelSerializer):
