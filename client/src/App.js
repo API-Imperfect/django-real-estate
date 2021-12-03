335959ded8761d964ea38c760c50733c1681c1f5
API Imperfect <alphaogilo@gmail.com>
2021-12-03 10:30:20 +0200

    chore: setup nginx and react with docker

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<h1>Real Estate</h1>
+			<p>The real test starts here..</p>
+		</div>
+	);
+}
+
+export default App;
